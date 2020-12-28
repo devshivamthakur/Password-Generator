bee2c8ee4459324fbe47615f0693603792a45a1b
shivam thakur <64252021+itsprogrammingwithme@users.noreply.github.com>
2020-12-28 20:42:45 +0530

    add copy to clipBoard feature

--- app/src/main/java/com/example/passwordgenerator/MainActivity.java
+++ app/src/main/java/com/example/passwordgenerator/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.passwordgenerator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ClipboardManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Adapter;
@@ -22,6 +23,8 @@ private String ps="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz012345678
 private     Integer len[]={4,5,6,7,8,9,10,11};
 private Spinner  spinner;
     private  int plength=4;
+    private boolean flag;
+    private  String np="";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +32,8 @@ private Spinner  spinner;
         set_password=findViewById(R.id.set_password);
         generate_password=findViewById(R.id.generate_password);
         copy_password=findViewById(R.id.copy_password);
+        copy_password.setEnabled(false);
+
         spinner=findViewById(R.id.spinner);
         ArrayAdapter<Integer> arrayAdapter=new ArrayAdapter<Integer>(this,R.layout.support_simple_spinner_dropdown_item,len);
         spinner.setAdapter(arrayAdapter);
@@ -39,12 +44,28 @@ private Spinner  spinner;
                 generate_password();
             }
         });
+        copy_password.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               copy_password();
+            }
+        });
+    }
+
+    private void copy_password() {
+        if(!np.isEmpty()){
+            ClipboardManager clipboardManager= (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+            clipboardManager.setText(np);
+            Toast.makeText(this, np + " copied ", Toast.LENGTH_SHORT).show();
+        }
     }
 
     private void generate_password() {
-
+    if(!flag){
+        copy_password.setEnabled(true);
+    }
         //new password
-        String np = "";
+        np = "";
         while (np.length()!=plength){
             np+=String.valueOf(ps.charAt(rp.nextInt(ps.length())));
         }
